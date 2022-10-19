f0aa068c63a0188e4690df87005b68182d844090
Xiaodong Li <1128xiaodong@gmail.com>
2022-10-19 11:04:45 -0700

    create WebServiceActivity

--- app/src/main/java/edu/northeastern/numad22fa_team12/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team12/MainActivity.java
@@ -2,13 +2,25 @@ package edu.northeastern.numad22fa_team12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int buttonID = view.getId();
+        switch (buttonID) {
+            case R.id.button_web_service:
+                startActivity(new Intent(MainActivity.this, WebServiceActivity.class));
+                break;
+        }
     }
 }
