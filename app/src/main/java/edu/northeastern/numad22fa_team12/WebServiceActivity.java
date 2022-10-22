2def5c515bfcce8b7d49f8bee464b7d0ab2eb0a5
ailin-zhang <ailinzhang0822@gmail.com>
2022-10-21 21:03:27 -0700

    main activity & ui design update

--- app/src/main/java/edu/northeastern/numad22fa_team12/WebServiceActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team12/WebServiceActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WebServiceActivity extends AppCompatActivity {
     Button searchButton;
+    EditText inputBrand;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,9 @@ public class WebServiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_service);
 
         searchButton = findViewById(R.id.search_button);
-
-
+        inputBrand = findViewById(R.id.input_brand);
     }
+
+
 
 }
