83d91a3c2752d096b1e48223ecad2666354f9637
ailin-zhang <ailinzhang0822@gmail.com>
2022-10-21 20:31:34 -0700

    main activity update

--- app/src/main/java/edu/northeastern/numad22fa_team12/WebServiceActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team12/WebServiceActivity.java
@@ -2,13 +2,22 @@ package edu.northeastern.numad22fa_team12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WebServiceActivity extends AppCompatActivity {
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_service);
+
+        searchButton = findViewById(R.id.search_button);
+
+
     }
+
 }
