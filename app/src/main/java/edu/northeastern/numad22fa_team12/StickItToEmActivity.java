55127680bdaef8636626064b03a550bbdb2f97c8
Xiaodong Li <1128xiaodong@gmail.com>
2022-11-03 16:28:25 -0700

    Add Firebase Database

--- app/src/main/java/edu/northeastern/numad22fa_team12/StickItToEmActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team12/StickItToEmActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class StickItToEmActivity extends AppCompatActivity {
@@ -17,6 +18,7 @@ public class StickItToEmActivity extends AppCompatActivity {
     private static final String SERVER_KEY = "AAAAn-7LHFg:APA91bFjKqRC26hJV6TSDwVKKmw2frTjAWPBne9-" +
             "SDObmZhYxgZDBNUOvqN4uYIap1flKY-VMT5NhSfpJ2prSPOEJ2L7_ucxU2ybsayIlExHbfcbUVrOMEL56DzbNhy3yTPTkVq8NHUl";
     private static String FCM_REGISTRATION_TOKEN;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,8 @@ public class StickItToEmActivity extends AppCompatActivity {
                         Log.d(TAG, msg);
                     }
                 });
+
+        database = FirebaseDatabase.getInstance();
     }
 
 }
