4a3d5e9c07aad1692aac44cf09e499fdf8e094ba
Xiaodong Li <1128xiaodong@gmail.com>
2022-11-05 22:09:56 -0700

    Update RecycleView to display stickers

--- app/src/main/java/edu/northeastern/numad22fa_team12/stickItToEm/StickItToEmActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team12/stickItToEm/StickItToEmActivity.java
@@ -9,6 +9,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,7 +22,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 import edu.northeastern.numad22fa_team12.R;
 
-public class StickItToEmActivity extends AppCompatActivity {
+public class StickItToEmActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "StickItToEmActivity";
     private static final String SERVER_KEY = "AAAAn-7LHFg:APA91bFjKqRC26hJV6TSDwVKKmw2frTjAWPBne9-" +
@@ -34,12 +36,14 @@ public class StickItToEmActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     private int[] stickersLocations;
     private int stickerSelected;
+    private ImageView imageSelected;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stick_it_to_em);
+        imageSelected = findViewById(R.id.imageView_test);
         createRecycleView();
 
         FirebaseMessaging.getInstance().getToken()
@@ -63,16 +67,17 @@ public class StickItToEmActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser curUser = userAuth.getCurrentUser();
-        if (curUser == null) {
-            // if user not register, take user to register page
-            startActivity(new Intent(StickItToEmActivity.this, RegisterActivity.class));
-        }
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        FirebaseUser curUser = userAuth.getCurrentUser();
+//        if (curUser == null) {
+//            // if user not register, take user to register page
+//            startActivity(new Intent(StickItToEmActivity.this, RegisterActivity.class));
+//        }
+//    }
 
+    @SuppressLint({"ResourceType", "UseCompatLoadingForDrawables"})
     private void createRecycleView() {
         stickersLocations = new int[]{R.drawable.cat, R.drawable.dog, R.drawable.duck, R.drawable.hedgehog,
                 R.drawable.koala, R.drawable.panda, R.drawable.pig, R.drawable.rooster};
@@ -80,7 +85,17 @@ public class StickItToEmActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.RecycleView_Stickers);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        stickerAdapter = new StickerAdapter(this, stickersLocations);
+        stickerAdapter = new StickerAdapter(this, stickersLocations, imageSelected);
         recyclerView.setAdapter(stickerAdapter);
+    }
+
+    public void sendMessage() {
+        stickerSelected = (int) getIntent().getSerializableExtra("id");
+        Log.i(TAG, "STICK ID" + stickerSelected);
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendMessage();
     }
 }
