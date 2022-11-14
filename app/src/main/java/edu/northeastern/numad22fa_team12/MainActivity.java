c1efb30f29029b036ed7112ed4fa894a2765863f
Xiaodong Li <1128xiaodong@gmail.com>
2022-11-14 14:41:16 -0800

    Create new activity for OutfitToday

--- app/src/main/java/edu/northeastern/numad22fa_team12/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team12/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import edu.northeastern.numad22fa_team12.outfitToday.OutfitToday;
 import edu.northeastern.numad22fa_team12.stickItToEm.RegisterActivity;
 import edu.northeastern.numad22fa_team12.stickItToEm.StickItToEmActivity;
 import edu.northeastern.numad22fa_team12.webservice.WebServiceActivity;
@@ -30,6 +31,9 @@ public class  MainActivity extends AppCompatActivity implements View.OnClickList
                 break;
             case R.id.button_About:
                 startActivity(new Intent(MainActivity.this, AboutActivity.class));
+                break;
+            case R.id.button_OutfitToday:
+                startActivity(new Intent(MainActivity.this, OutfitToday.class));
                 break;
         }
     }
---
+++ app/src/main/java/edu/northeastern/numad22fa_team12/outfitToday/OutfitToday.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad22fa_team12.outfitToday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numad22fa_team12.R;
+
+public class OutfitToday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_outfit_today);
+    }
+}
