45f8eaadf5e7b092c592f91a2287afce491e7e0f
KylieLLL <xu.yuanh@northeastern.edu>
2022-12-02 14:40:28 -0800

    add edit outfit activity

---
+++ app/src/main/java/edu/northeastern/numad22fa_team12/outfitToday/EditOutfitActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad22fa_team12.outfitToday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditOutfitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_outfit);
+    }
+}
