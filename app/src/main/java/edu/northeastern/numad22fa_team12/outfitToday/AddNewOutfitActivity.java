97a739d167e1f3f0339de5ad5c472b687436d81e
sirancao <cao.sir@northeastern.edu>
2022-12-07 14:59:30 -0800

    delete add to all category

--- app/src/main/java/edu/northeastern/numad22fa_team12/outfitToday/AddNewOutfitActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team12/outfitToday/AddNewOutfitActivity.java
@@ -185,7 +185,6 @@ public class AddNewOutfitActivity extends AppCompatActivity {
                 userRef.child(userId).child("wardrobe").child(imageId).setValue(item);
                 userRef.child(userId).child("categoryList").child(category).child("occasion").child(occasion).child(imageId).setValue(web_uri);
                 userRef.child(userId).child("categoryList").child(category).child("season").child(season).child(imageId).setValue(web_uri);
-                userRef.child(userId).child("categoryList").child(category).child("season").child("all").child(imageId).setValue(web_uri);
                 finish();
 
             }
